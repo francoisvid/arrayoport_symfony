7b789d45eea43daf55c348f3ae9f6ebbbd562da4
Vidal Francois <vidalfrancois11@gmail.com>
2018-07-26 09:45:58 +0200

    caca

--- webpack.config.js
+++ webpack.config.js
@@ -15,8 +15,16 @@ Encore
     .enableVersioning(Encore.isProduction())
 
     // uncomment to define the assets of the project
-    //.addEntry('js/app', './assets/js/app.js')
-    //.addStyleEntry('css/app', './assets/css/app.scss')
+    .addEntry('js/admin/admin', './assets/js/admin.js')
+    .addEntry('js/morris/morris', './assets/morris/morris.js')
+    .addEntry('js/bootstrap/bootstrap', './assets/bootstrap/bootstrap.js')
+
+    .addStyleEntry('css/admin/admin', './assets/css/admin.css')
+    .addStyleEntry('css/barnav/barnav', './assets/css/barnav.css')
+    .addStyleEntry('css/morris/morris', './assets/morris/morris.css')
+    .addStyleEntry('css/bootstrap/bootstrap', './assets/bootstrap/bootstrap.css')
+    .addStyleEntry('css/theme/sb-admin', './assets/css/sb-admin.css')
+    // .addStyleEntry('css/theme/sb-admin.min', './assets/css/sb-admin.min.css')
 
     // uncomment if you use TypeScript
     //.enableTypeScriptLoader()
@@ -26,6 +34,8 @@ Encore
 
     // uncomment for legacy applications that require $/jQuery as a global variable
     //.autoProvidejQuery()
+
+
 ;
 
 module.exports = Encore.getWebpackConfig();
